14f825bcf0fb9eca6b98e7cd4a17e1a7d35cbd68
GrowlingViking <siggesg@hotmail.com>
2018-01-17 13:25:54 +0100

    Added Trafikklys class

---
+++ WindowsProject1/WindowsProject1/Trafikklys.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Trafikklys.h"
+
+
+Trafikklys::Trafikklys(HDC hdc, int left, int top, int right, int bottom, int hState) {
+
+}
+
+
+Trafikklys::~Trafikklys() {
+
+}
---
+++ WindowsProject1/WindowsProject1/Trafikklys.h
@@ -0,0 +1,11 @@
+#pragma once
+class Trafikklys {
+
+public:
+
+	Trafikklys(HDC hdc, int left, int top, int right, int bottom, int hState);
+
+	~Trafikklys();
+
+};
+
