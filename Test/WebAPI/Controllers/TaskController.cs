c866dc04b51ac784f27f7985e3f5eb94a4cb0ce8
GrowlingViking <siggesg@hotmail.com>
2018-02-19 16:39:45 +0100

    App testing: Updated the TaskController

--- Test/WebAPI/Controllers/TaskController.cs
+++ Test/WebAPI/Controllers/TaskController.cs
@@ -4,36 +4,43 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPI.Models;
+using Newtonsoft.Json;
 
 namespace WebAPI.Controllers
 {
-    public class ValuesController : ApiController
+    public class TaskController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<Task> Get()
         {
-            return new string[] { "value1", "value2" };
+            return TaskModel.GetAllTasks();
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public Task Get(int id)
         {
-            return "value";
+            return TaskModel.GetTask(id);
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            var task = JsonConvert.DeserializeObject<Task>(value);
+            TaskModel.CreateTask(task);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            var task = JsonConvert.DeserializeObject<Task>(value);
+            TaskModel.UpdateTask(id, task);
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            TaskModel.DeleteTask(id);
         }
     }
 }
